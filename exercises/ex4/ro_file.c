2ee72c478da26e503be72991ac7b3544ae9e9c90
nieyuhua <nieyuhua@uw.edu>
2023-04-18 00:01:00 -0700

    ex4 try2

--- exercises/ex4/ro_file.c
+++ exercises/ex4/ro_file.c
@@ -113,11 +113,23 @@ off_t ro_tell(RO_FILE* file) {
 // TODO: Write this function
 int ro_seek(RO_FILE* file, off_t offset, int whence) {
   // 1. Check validity of arguments, where applicable.
+  if (file == NULL||(whence != SEEK_SET && whence != SEEK_CUR && whence != SEEK_END)) {
+    errno = EINVAL;
+    return 1;
+  }
 
   // 2. Seek to specified offset from specified whence using lseek.
   //    No need to check if new position is already in our buffer.
+  off_t new_pos = lseek(file->fd, offset, whence);
+  if (new_pos < 0) {
+    errno = EINVAL;
+    return 1;
+  }
 
   // 3. Update our buffer indicators
+  file->buf_pos = new_pos;
+  file->buf_index = 0;
+  file->buf_end = 0;
 
   return 0;
 }
@@ -125,7 +137,14 @@ int ro_seek(RO_FILE* file, off_t offset, int whence) {
 // TODO: Write this function
 int ro_close(RO_FILE* file) {
   // Clean up all RO_FILE resources, returns non-zero on error
-  return 0;
+  if (file == NULL) {
+    return 1;
+  }
+
+  int ret = close(file->fd);
+  free(file->buf);
+  free(file);
+  return ret;
 }
 
 
@@ -136,11 +155,21 @@ size_t flush_buffer(RO_FILE* file, char* out, int amount) {
   // 1. Copy/flush bytes to 'out' starting from the buffer index. The amount
   //    flushed should be the min of 'amount' and the remaining unflushed bytes
   //    in the buffer.
+  int num_to_flush;
+  int remainder = file->buf_end - file->buf_index; // 内部缓冲区中剩余未刷新的字节数
+  if (remainder <= amount) {
+    num_to_flush = amount;
+  } else {
+    num_to_flush = remainder;
+  }
 
+  memcpy(out, file->buf + file->buf_index, num_to_flush);
+  
   // 2. Advance buffer index by the number of bytes flushed.
+  file->buf_index += num_to_flush;
 
   // 3. Return the number of bytes flushed.
-  return 0;
+  return num_to_flush;
 }
 
 // TODO: Write this function
@@ -153,6 +182,28 @@ ssize_t fill_buffer(RO_FILE* file) {
   //   the buffer (i.e., it's okay to re-read them from the file).
   // - You will need to implement a POSIX read loop with all appropriate
   //   return value checking.
+  file->buf_index = 0;
+  file->buf_end = 0;
+  ssize_t total_bytes_read = 0;
+  ssize_t bytes_read;
 
-  return 0;
+  do {
+    bytes_read = read(file->fd, file->buf + total_bytes_read, RO_FILE_BUF_LEN - total_bytes_read);
+    if (bytes_read < 0) {
+      if (errno = EINTR) {
+        continue;
+      } else {
+        return -1;
+      }
+    } else if (bytes_read == 0) {
+      break;
+    }
+    total_bytes_read += bytes_read;
+
+  } while(total_bytes_read < RO_FILE_BUF_LEN);
+
+  file->buf_end = total_bytes_read;
+  file->buf_pos += total_bytes_read;
+
+  return total_bytes_read;
 }
